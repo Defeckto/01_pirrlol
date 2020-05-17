58f5aabdc67f402a5aa485904eae98c2d69b51d6
Slivinskyi Yevhenii <ffrostqs@gmail.com>
2020-05-18 01:15:21 +0300

    Fix bug: button 'load more'

--- js/load-more.js
+++ js/load-more.js
@@ -1,18 +1,19 @@
 $(document).ready(function () {
-	let x = $(".work__row.work__item");
+	let x = $(".work__item").length;
+
 	$(".work__item").slice(0, 4).show();
+
 	$("#loadMore").on("click", function (e) {
 		e.preventDefault();
 		$(".work__item:hidden").slice(0, 4).slideDown();
-		console.log($(".work__item"), 'asdada')
+		console.log(x)
+		if (x !== $(".work__item").length ) {
+			$("#loadMore").text('Load more work');
+		}
 
 		if ($(".work__item:hidden").length === 0) {
 
 			$("#loadMore").addClass("noContent");
-
-		}
-		// console.log(x.length, 'sss')
-		if(x.length === 4){
 
 		}
 	});
